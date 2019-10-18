60b76d5d2ea344d51d3ee4b5e5249416b24f5075
nelsonfassis <nelson.f.assis@gmail.com>
2019-10-17 21:52:13 -0700

    Add form to create products

--- products/models.py
+++ products/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     summary     = models.TextField(blank=False, null=False)
-    featured    = models.BooleanField()
+    featured    = models.BooleanField(default=False)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-
+from .forms import ProductCreateForm
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
@@ -13,3 +13,15 @@ def product_detail_view(request):
         'object': obj
     }
     return render(request, "products/products_detail.html", context)
+
+def product_create_view(request):
+    form = ProductCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductCreateForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "products/products_create.html", context)
