e40cbb2d0bbfdbd05d02e83c2780a17e1d3223bb
nelsonfassis <nelson.f.assis@gmail.com>
2019-10-20 15:29:37 -0700

    Adds some style to navbar

--- products/views.py
+++ products/views.py
@@ -4,11 +4,7 @@ from .forms import ProductCreateForm
 # Create your views here.
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
-    # context = {
-    #     'title': obj.title,
-    #     'description': obj.description,
-    #     'price': obj.price
-    # }
+    
     context = {
         'object': obj
     }
